f2dc74f2b23bfbade7f6a2b88ff108f01d7b7743
hieu4399243 <hieu243268@gmail.com>
2023-06-05 21:35:39 +0700

    add towers

---
+++ TowerDefender/Assets/Prefabs/UI/HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+	public Slider slider;
+	public Gradient gradient;
+	public Image fill;
+
+	public GameObject slow;
+
+	public GameObject burning;
+    
+
+	public void SetMaxHealth(float health)
+	{
+		slider.maxValue = health;
+		slider.value = health;
+
+		fill.color = gradient.Evaluate(1f);
+	}
+
+    public void SetHealth(float health)
+	{
+		slider.value = health;
+
+		fill.color = gradient.Evaluate(slider.normalizedValue);
+	}
+
+	public void SetStatus(string status)
+    {
+        if (status.Equals("slow"))
+        {
+			slow.SetActive(true);
+
+        }else if (status.Equals("burning"))
+        {
+			burning.SetActive(true);
+        }
+    }
+    
+
+	public void DisableStatus(string status)
+	{
+		if (status.Equals("slow"))
+		{
+			slow.SetActive(false);
+
+		}
+		else if (status.Equals("burning"))
+		{
+			burning.SetActive(false);
+		}
+	}
+
+}
